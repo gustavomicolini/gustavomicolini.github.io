c9863d30f0b13dd3a41c529f1a118328d41b0ec4
gustavomicolini <micolinigustavo@gmail.com>
2022-03-18 20:27:19 -0300

    server frontend gussocketio

---
+++ js/Index-scripts.js
@@ -0,0 +1,64 @@
+//Funciones para los click de botones
+function cargoDato() {
+	pidoDato();
+	document.getElementById("demo").innerHTML = "Valor bitstamp actualizado!!!!";
+	document.getElementById("bitstamp").innerHTML = "Valor=xxxx!!!!";
+}
+function reiniDato() {
+	document.getElementById("demo").innerHTML = "Click the button para actualizar valor de bitstamp.";
+	document.getElementById("bitstamp").innerHTML = "0";
+	document.getElementById("demo4").innerHTML = 'sin datos';
+	document.getElementById("demo5").innerHTML = 'sin dato';
+}
+function pidoDato() {
+	let textoenvio = {
+		usuario:'usuario',
+		mensaje:'Hola ServerIO, mandame dato'
+		};
+	
+	//emit = cliente envia informacion solo al servidor, otro cliente no lo recibe
+	socket.emit('enviarMensaje',textoenvio , function(respuesta) { //respuesta del callback
+		//Imprimo texto enviado
+		console.log('usuario, ',textoenvio.mensaje);
+		document.getElementById("demo4").innerHTML = textoenvio.mensaje;
+		//Imprimo respuesta
+		console.log('ServerIO:', respuesta);
+		const myJSON = JSON.stringify(respuesta);
+		document.getElementById("demo5").innerHTML = myJSON;
+	});
+}
+
+
+
+//Inicio conexion al servidor mediante socket.io
+//var socket = io(); //cuando cliente en el mismo Node.js
+var socket = io('https://gusocketio-backend.herokuapp.com'); //cliente en otro servidor
+
+socket.on('connect',function(){
+    console.log('Nuevo clienteWEB, frontend pide CONECTARSE al socket.io') 
+	document.getElementById("demo3").innerHTML = 'Nuevo clienteWEB, frontend pide CONECTARSE al socket.io';
+});
+
+socket.on('disconnect',function(){
+    console.log('clienteWEB, frontend pide DES-CONECTARSE del socket.io') 
+	document.getElementById("demo3").innerHTML = 'clienteWEB, frontend pide DES-CONECTARSE del socket.io';
+});
+
+//escuchar al servidorIO
+socket.on('enviarMensaje',function(mensaje){
+	//Imprimo respuesta
+    console.log('ServerIO:', mensaje);
+	const myJSON = JSON.stringify(mensaje);
+	document.getElementById("demo5").innerHTML = myJSON;
+});	
+
+
+//recibiendo un numero random cada dos segundos
+socket.on('server/random', function(num){
+    //console.log(num)
+	document.getElementById("bitstamp").innerHTML = num;
+})
+//enviando un numero random cada dos segundos
+//setInterval(function(){
+//    socket.emit('client/random', Math.random())
+//}, 2000)
