d10d80d8a649f61c94115a4fc948ab4aa5b3c51a
gustavomicolini <micolinigustavo@gmail.com>
2022-03-26 10:32:05 -0300

    index

--- js/Index-scripts.js
+++ js/Index-scripts.js
@@ -2,20 +2,12 @@
 
 //Inicio conexion al servidor mediante socket.io
 //var socket = io(); //cuando cliente en el mismo Node.js
-var socket = io.connect('https://gusphpsocketio-b.herokuapp.com', { 'forceNew': true });
+var socket = io.connect('https://gusmonitor-back.herokuapp.com', { 'forceNew': true });
 
 //----------------------------------------------------------------
 /*$ io es el objeto SocketIO. $ socket es la conexión del cliente
  $ datos pueden ser números y cadenas, o una matriz. Cuando $ data es una matriz, el cliente la convertirá automáticamente en un objeto javascript.
  De la misma manera, si el cliente envía un objeto javascript a un evento emitido por el servidor, se convertirá automáticamente a una matriz php cuando el servidor lo reciba.*/
-//----------------------------------------------------------------
-/*socket.emit('connect', 'ServerPHP: Cliente conectado al socket.io'); //mando al backend al inicio
-
-socket.on('resp connect', function(data){ //recibo del backend
-	document.getElementById("estado").innerHTML = data;
-});
-*/
-
 //---------------------------------------------------------------
 $(document).ready(function(){
 socket.emit('send connect', 'Cliente conectado al socket.io'); //mando al backend al inicio
